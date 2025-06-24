e1dec6645388abfc2f5542b217e9e46eb143ac30
EveHackmon <157503186+EveHackmon@users.noreply.github.com>
2025-06-24 23:59:30 +0300

    Frontend fixes

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,9 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  // baseURL: 'https://flights-backend-1085375047232.us-central1.run.app',
-  // baseURL: 'http://backend:8080',
+  //baseURL: 'http://localhost:8080',
+  baseURL: 'https://flights-backend-1085375047232.us-central1.run.app',
 });
 
 api.interceptors.request.use(
