0ddace6ec24ad98392212b15aa97ddef6c47f1c7
EveHackmon <157503186+EveHackmon@users.noreply.github.com>
2025-06-24 16:43:25 +0300

    flights test updated2

--- Backend/tests/flights.spec.js
+++ Backend/tests/flights.spec.js
@@ -30,13 +30,12 @@ describe('Flight Test', () => {
 
   it('should return the correct flight from search-flights query', async () => {
   const res = await request(app)
-    .get('/flights/search-flights')
-    .query(new URLSearchParams({
+    .get(`/flights/search-flights?${new URLSearchParams({
       origin: 'Tel Aviv',
       destination: 'New York',
       departure_date: '2025-07-10',
       passengers: 1
-    }).toString())   
+    }).toString()}`)   
     .expect(200);
 
     expect(res.body).to.be.an('array').that.is.not.empty;
