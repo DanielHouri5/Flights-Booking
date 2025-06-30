14db636ce56f20eedcc4b55735122c33a05130d7
DanielHouri5 <hdanielh50@gmail.com>
2025-06-30 16:09:22 +0300

    Update docker-compose.prod.yml

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,9 +1,10 @@
 // Frontend/src/services/api.js
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_SERVICE_URL || 'http://localhost:8080';
+
 const api = axios.create({
-  // baseURL: 'http://localhost:8080',
-  baseURL: 'https://flights-backend-1085375047232.us-central1.run.app',
+  baseURL: BASE_URL,
 });
 
 api.interceptors.request.use(
