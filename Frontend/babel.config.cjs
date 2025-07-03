64a49de7d87b0fa9cbb5cd994f6f24f1dfce1242
DanielHouri5 <hdanielh50@gmail.com>
2025-07-03 20:36:20 +0300

    Add documentation and update README

--- Frontend/babel.config.cjs
+++
@@ -1,7 +0,0 @@
-// Babel configuration file for the React frontend
-// This file tells Babel which presets to use for transpiling the code
-// '@babel/preset-env' enables support for the latest JavaScript features
-// '@babel/preset-react' enables support for JSX syntax
-export default {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-};
