ed3f100da669458af29d62378a6d8893508dfe18
DanielHouri5 <hdanielh50@gmail.com>
2025-07-03 21:05:03 +0300

    Fix backend tests

--- Backend/babel.config.cjs
+++ Backend/babel.config.cjs
@@ -1,6 +1,6 @@
 // Babel configuration file for the Backend project
 // This configures Babel to use the @babel/preset-env preset
 // with the target set to the current Node.js version (for Jest and Node compatibility)
-export default {
+module.exports = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
 };
