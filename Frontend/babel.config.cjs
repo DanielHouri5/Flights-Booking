648cfc2c2111c7c2f2f05d35c3a05e39650337ea
DanielHouri5 <hdanielh50@gmail.com>
2025-07-03 21:06:05 +0300

    Fix frontend tests

--- Frontend/babel.config.cjs
+++ Frontend/babel.config.cjs
@@ -2,6 +2,6 @@
 // This file tells Babel which presets to use for transpiling the code
 // '@babel/preset-env' enables support for the latest JavaScript features
 // '@babel/preset-react' enables support for JSX syntax
-export default {
+module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
 };
